8dccc85ed4f41a9e046fb7bd43d08ef06dccb068
Tommi Timonen <timonen@outlook.com>
2017-12-04 21:09:23 +0200

    update

--- claymore/mine.sh
+++ claymore/mine.sh
@@ -9,5 +9,3 @@ export GPU_USE_SYNC_OBJECTS=1
 export GPU_MAX_ALLOC_PERCENT=50
 
 export GPU_SINGLE_ALLOC_PERCENT=50
-
-./ethdcrminer64 -epool eu1.ethermine.org:4444 -ewal 0xd458dc7cF5412ae171905572E09c7eEe87e1f6DA.orja$RANDOM -epsw x -mode 1 -tt 68 -allpools 1
