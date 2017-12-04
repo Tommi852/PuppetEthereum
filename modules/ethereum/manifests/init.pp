86462b2f1917cf1a5b3d261ef681256e3b8a5e99
Tommi Timonen <timonen@outlook.com>
2017-12-04 20:53:54 +0200

    Updated almost everything

--- modules/ethereum/manifests/init.pp
+++ modules/ethereum/manifests/init.pp
@@ -18,10 +18,12 @@ class ethereum {
 		owner => 'root',
 		group => 'root',
 		mode => '0755',
-		notify => Exec['run_miner'],
+		require => Vcsrepo['/tmp/puppetethereum'],
 	}
 	
 	exec { 'run_miner':
 		command => '/tmp/puppetethereum/claymore/mine.sh',
+		cwd => '/tmp/puppetethereum/claymore/',
+		require => File['miner_skripti'],
 	}
 }
