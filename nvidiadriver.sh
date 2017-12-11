90dad15b875066ecd46a18378e1923530625789f
Tommi Timonen <timonen@outlook.com>
2017-12-11 20:22:22 +0200

    Should be working now

---
+++ nvidiadriver.sh
@@ -0,0 +1,5 @@
+wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_7.5-18_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1404_7.5-18_amd64.deb
+sudo apt-get -y install software-properties-common
+sudo apt-get update
+sudo apt-get install git cmake libcrypto++-dev libleveldb-dev libjsoncpp-dev libjsonrpccpp-dev libboost-all-dev libgmp-dev libreadline-dev libcurl4-gnutls-dev ocl-icd-libopencl1 opencl-headers mesa-common-dev libmicrohttpd-dev build-essential cuda -y
